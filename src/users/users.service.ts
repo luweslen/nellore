2953a12217105b5d4c32c795cb92598b63992d59
Luciano Weslen <luciano.weslen@zrp.com.br>
2022-06-01 19:31:14 -0300

    feat: added module users

---
+++ src/users/users.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
+import { UserRole } from './enum/user-roles.enum';
+import { UserRepository } from './user.repository';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {}
+
+  async createAdminUser(createUserDto: CreateUserDto): Promise<User> {
+    if (createUserDto.password !== createUserDto.passwordConfirmation) {
+      throw new UnprocessableEntityException('As senhas não conferem');
+    } else {
+      return this.userRepository.createUser(createUserDto, UserRole.ADMIN);
+    }
+  }
+}
